be5f14c403ed524bd3a7303046d5537cc37680f2
Vincent C <github@fleeto.us>
2021-01-13 14:35:19 +0800

    fixed: merged context/rbac-namespace/sample-workload

--- auto-labeler.py
+++ auto-labeler.py
@@ -27,22 +27,25 @@ if context[0]["type"] == "Synchronization":
     print("Garbage fired.")
     sys.exit(0)
 
-obj = context[0]["object"]
+for item in context:
+    obj = item["object"]
 
-node_name = obj["spec"]["nodeName"]
-namespace = obj["metadata"]["namespace"]
-pod_name = obj["metadata"]["name"]
+    node_name = obj["spec"]["nodeName"]
+    namespace = obj["metadata"]["namespace"]
+    pod_name = obj["metadata"]["name"]
+    print("Processing {}...".format(pod_name))
 
-node_info = json.loads(
-    subprocess.check_output(
-        ["kubectl", "get", "node", node_name, "-o", "json"]))
+    node_info = json.loads(
+        subprocess.check_output(
+            ["kubectl", "get", "node", node_name, "-o", "json"]))
 
-node_labels = node_info["metadata"]["labels"]
-copy_keys = COPY_LABELS.split(",")
+    node_labels = node_info["metadata"]["labels"]
+    copy_keys = COPY_LABELS.split(",")
 
-final_labels = []
-for key in [key for key in node_labels.keys() if key in copy_keys]:
-    final_labels.append("{}={}".format(key, node_labels[key]))
-cmd = ["kubectl", "label", "pods", pod_name,  "-n", namespace] + final_labels
-print(subprocess.check_output(cmd))
+    final_labels = []
+    for key in [key for key in node_labels.keys() if key in copy_keys]:
+        final_labels.append("{}={}".format(key, node_labels[key]))
+    cmd = ["kubectl", "label", "pods", pod_name,  "-n", namespace] + final_labels
+    print(cmd)
+    print(subprocess.check_output(cmd))
 sys.exit(0)
